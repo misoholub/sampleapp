33e1ef63585e6d7cfd8390d2682e382098719536
Michal Holub <miso.holub@gmail.com>
2013-08-26 15:06:13 +0200

    Refactor test to verify the content more precisly by examining the content of a specific html tag

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 describe 'Static pages' do
 
   subject { page }
+  shared_examples_for 'all static pages' do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe 'Home page' do
     before(:each) { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like 'all static pages'
     it { should_not have_title('| Home') }
   end
 
