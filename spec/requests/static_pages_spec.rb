c829b5a93cb2d3dfdeb87f3bd7c41a4ad118afcb
Michal Holub <miso.holub@gmail.com>
2013-08-26 15:15:14 +0200

    Add testing of the proper page displayed after clicking a link

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,22 @@ describe 'Static pages' do
     it { should have_title(full_title(page_title)) }
   end
 
+  it 'should have the right links on the layout' do
+    visit root_path
+    click_link 'About'
+    expect(page).to have_title(full_title('About'))
+    click_link 'Help'
+    expect(page).to have_title(full_title('Help'))
+    click_link 'Contact'
+    expect(page).to have_title(full_title('Contact'))
+    click_link 'Home'
+    expect(page).to have_title(full_title(''))
+    click_link 'Sign up now!'
+    expect(page).to have_title(full_title('Sign up'))
+    click_link 'sample app'
+    expect(page).to have_title(full_title(''))
+  end
+
   describe 'Home page' do
     before(:each) { visit root_path }
     let(:heading) { 'Sample App' }
