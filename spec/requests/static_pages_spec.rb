3abe69f9f028bf820d14f7d7231c8bc380588738
Michal Holub <miso.holub@gmail.com>
2013-08-26 14:43:00 +0200

    Double quotes changed to single quotes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe 'Static pages' do
 
   subject { page }
 
-  describe "Home page" do
+  describe 'Home page' do
     before(:each) { visit root_path }
 
     it { should have_content('Sample App') }
@@ -12,21 +12,21 @@ describe "Static pages" do
     it { should_not have_title('| Home') }
   end
 
-  describe "Help page" do
+  describe 'Help page' do
     before(:each) { visit help_path }
 
     it { should have_content('Help') }
     it { should have_title(full_title('Help')) }
   end
 
-  describe "About page" do
+  describe 'About page' do
     before(:each) { visit about_path }
 
   	it { should have_content('About') }
     it { should have_title(full_title('About')) }
   end
 
-  describe "Contact page" do
+  describe 'Contact page' do
     before(:each) { visit contact_path }
 
     it { should have_content('Contact') }
