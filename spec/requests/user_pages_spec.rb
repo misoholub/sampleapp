d9ba5b7f1f5bc6640897b8e807d44d5951d7b9c7
Michal Holub <miso.holub@gmail.com>
2013-09-06 12:34:25 +0200

    Add test of success page after valid signup attempt

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,6 +48,15 @@ describe 'UserPages' do
       it 'should create a user' do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'michal@test.example.sk') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcomeee') }
+      end
+
     end
 
   end
