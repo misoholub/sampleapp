7573b18aaee98e44b297e44ecf5c70fd4d9c6ddf
Michal Holub <miso.holub@gmail.com>
2013-09-10 11:20:52 +0200

    Add tests verifying that not-signed-in user doesnt see links to settings, profile, sign out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,9 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+    it { should_not have_link('Sign out') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
   end
 
   describe 'signin' do
@@ -40,6 +43,9 @@ describe "Authentication" do
       describe 'followed by signout' do
         before { click_link 'Sign out' }
         it { should have_link('Sign in') }
+        it { should_not have_link('Sign out') }
+        it { should_not have_link('Profile') }
+        it { should_not have_link('Settings') }
       end
 
     end
