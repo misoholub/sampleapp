af74125416e6a49967fef5ee43ee2ffeb9cd0a76
Michal Holub <miso.holub@gmail.com>
2013-09-10 11:29:17 +0200

    Refactor new and edit user form, remove duplicate code into partial

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -14,10 +14,10 @@ def valid_signin(user, options={})
 end
 
 def valid_signup
-  fill_in 'Name',         with: 'Example User'
-  fill_in 'Email',        with: 'user@example.com'
-  fill_in 'Password',     with: 'foobar'
-  fill_in 'Confirmation', with: 'foobar'
+  fill_in 'Name',             with: 'Example User'
+  fill_in 'Email',            with: 'user@example.com'
+  fill_in 'Password',         with: 'foobar'
+  fill_in 'Confirm password', with: 'foobar'
 end
 
 RSpec::Matchers.define :have_error_message do |message|
